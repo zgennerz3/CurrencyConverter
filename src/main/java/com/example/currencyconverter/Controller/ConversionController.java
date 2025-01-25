ddc234c7b2123898266a0963a0cb2e2f43aabe63
z_gennerz3 <zanegenrich@gmail.com>
2025-01-25 17:08:35 +1000

    Added basic controller logic

--- src/main/java/com/example/currencyconverter/Controller/ConversionController.java
+++ src/main/java/com/example/currencyconverter/Controller/ConversionController.java
@@ -1,7 +1,17 @@
 package com.example.currencyconverter.Controller;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ConversionController {
+    @FXML private TextField CurrencyInput;
+    @FXML private TextField CurrencyOutput;
+
+    @FXML
+    private void OnConvertButtonClick() {
+        String fromCurrencyInput = CurrencyInput.getText();
+        String toCurrencyInput = CurrencyOutput.getText();
+    }
 }
