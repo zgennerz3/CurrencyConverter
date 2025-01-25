7782c9225b0bab7422b26277ca1d3eacae004f4f
z_gennerz3 <zanegenrich@gmail.com>
2025-01-25 16:17:18 +1000

    Project setup continued...yet again

--- src/main/java/com/example/currencyconverter/Controller/ConversionController.java
+++ src/main/java/com/example/currencyconverter/Controller/ConversionController.java
@@ -4,11 +4,4 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class ConversionController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
--- src/main/java/com/example/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/example/currencyconverter/CurrencyConverter.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class CurrencyConverter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CurrencyConverter.class.getResource("conversion-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CurrencyConverter.class.getResource("View/conversion-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("Currency Converter");
         stage.setScene(scene);
