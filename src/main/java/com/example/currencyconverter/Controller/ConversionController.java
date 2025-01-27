d31bbe1a041e08701174d42275a6883b9a14c2c9
z_gennerz3 <zanegenrich@gmail.com>
2025-01-27 20:42:53 +1000

    Implemented basic sanity checking on inputs and removed trailing zeros from output

--- src/main/java/com/example/currencyconverter/Controller/ConversionController.java
+++ src/main/java/com/example/currencyconverter/Controller/ConversionController.java
@@ -17,32 +17,44 @@ public class ConversionController {
         // Get input text from the GUI
         String fromCurrencyInput = CurrencyFrom.getText();
         String toCurrencyInput = CurrencyTo.getText();
-        try {
-            Pair<String, Integer> APIResponse = APIHandling.APICalls.GetConversionByCurrencies(
-                    fromCurrencyInput, toCurrencyInput);
-            // Switch on the HTTP code returned by the API call
-            switch (APIResponse.getValue()) {
-                case 200: // successful case
-                    double conversionRate = APIHandling.APIProcessing.GetConversionRate(
-                            APIResponse.getKey(), toCurrencyInput);
-                    if (conversionRate == -1.0) {
-                        ResultsText.setText("The output currency entered is not supported");
-                    } else {
-                        ResultsText.setText(String.format("The conversion rate from %s to %s is %f",
-                                fromCurrencyInput.toUpperCase(), toCurrencyInput.toUpperCase(), conversionRate));
-                    } break;
-                case 404: // invalid currency(s) case
-                    ResultsText.setText("The conversion entered is not supported"); break;
-                case 422: // both currency case
-                    ResultsText.setText(String.format("The conversion from %s to %s is 1...obviously",
-                            fromCurrencyInput.toUpperCase(), toCurrencyInput.toUpperCase())); break;
-                default: // unexpected error(s)
-                    ResultsText.setText("An unexpected error occurred");
-            }
+
+        // Check if inputs are sane (3 alphabetical letter)
+        if (fromCurrencyInput.length() != 3 || toCurrencyInput.length() != 3
+            || !fromCurrencyInput.matches("[a-zA-Z]+") || !toCurrencyInput.matches("[a-zA-Z]+")) {
+            ResultsText.setText("Please enter valid currency codes");
         }
-        catch (Exception e) {
-            ResultsText.setText("An unexpected error occurred, " +
-                    "check your internet connection and try again");
+        else {
+            // Attempt to perform currency conversion
+            try {
+                Pair<String, Integer> APIResponse = APIHandling.APICalls.GetConversionByCurrencies(
+                        fromCurrencyInput, toCurrencyInput);
+                // Switch on the HTTP code returned by the API call
+                switch (APIResponse.getValue()) {
+                    case 200: // successful case
+                        double conversionRate = APIHandling.APIProcessing.GetConversionRate(
+                                APIResponse.getKey(), toCurrencyInput);
+                        if (conversionRate == -1.0) {
+                            ResultsText.setText("The output currency entered is not supported");
+                        } else {
+                            ResultsText.setText(String.format("The conversion rate from %s to %s is %f",
+                                    fromCurrencyInput.toUpperCase(), toCurrencyInput.toUpperCase(), conversionRate)
+                                    .replaceAll("\\.?0+$", "")); // remove trailing zeros
+                        }
+                        break;
+                    case 404: // invalid currency(s) case
+                        ResultsText.setText("The conversion entered is not supported");
+                        break;
+                    case 422: // both currency case
+                        ResultsText.setText(String.format("The conversion from %s to %s is 1...obviously",
+                                fromCurrencyInput.toUpperCase(), toCurrencyInput.toUpperCase()));
+                        break;
+                    default: // unexpected error(s)
+                        ResultsText.setText("An unexpected error occurred");
+                }
+            } catch (Exception e) {
+                ResultsText.setText("An unexpected error occurred, " +
+                        "check your internet connection and try again");
+            }
         }
     }
 }
