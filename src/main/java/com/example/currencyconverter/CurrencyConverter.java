f683028478dd559686c304324625b21c809c0212
z_gennerz3 <zanegenrich@gmail.com>
2025-02-17 18:25:20 +1000

    Implemented threading to quicken setup before launch

--- src/main/java/com/example/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/example/currencyconverter/CurrencyConverter.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 
 public class CurrencyConverter extends Application {
     @Override
@@ -30,7 +31,8 @@ public class CurrencyConverter extends Application {
             DAO.CreateTable();
         }
         catch (SQLException | RuntimeException e) {
-            System.err.println("Error in application startup (database setup)" + e.getMessage());
+            System.err.println("Error in application startup (database setup): " + e.getMessage());
+            System.exit(1);
         }
 
         // Update (or populate) the database with valid conversion pairs before launch,
@@ -47,18 +49,42 @@ public class CurrencyConverter extends Application {
             for (String toCurrency : validCurrenciesEUR) {
                 DAO.InsertIgnore("EUR", toCurrency);
             }
-            // Retrieve and store every other valid conversion based on the valid EUR conversions
-            for (String nextCurrency : validCurrenciesEUR) {
-                String aPIResponseBody = APIHandling.APICalls.GetValidConversions(nextCurrency);
-                ArrayList<String> validCurrencies = (ArrayList<String>)
-                        APIHandling.APIProcessing.ParseValidConversions(aPIResponseBody);
-                for (String toCurrency : validCurrencies) {
-                    DAO.InsertIgnore(nextCurrency, toCurrency);
-                }
+
+            // Retrieve and store every other valid conversion based on the valid 'EUR to ...' currencies,
+            // running each call/store method in parallel using threads
+            @SuppressWarnings("unchecked") // every thread here is void, warning safe to ignore
+            CompletableFuture<Void>[] aPICallStoreThreads = new CompletableFuture[validCurrenciesEUR.size()];
+            ArrayList<Throwable> threadErrors = new ArrayList<>();
+            for (int i = 0; i < validCurrenciesEUR.size(); i++) {
+                String currencyToCall = validCurrenciesEUR.get(i);
+                aPICallStoreThreads[i] = CompletableFuture.runAsync(() -> {
+                    try {
+                        String aPIResponseBody = APIHandling.APICalls.GetValidConversions(currencyToCall);
+                        ArrayList<String> validCurrencies = (ArrayList<String>)
+                                APIHandling.APIProcessing.ParseValidConversions(aPIResponseBody);
+                        for (String toCurrency : validCurrencies) {
+                            DAO.InsertIgnore(currencyToCall, toCurrency);
+                        }
+                    } catch (Exception e) { // Log exception(s), preventing data races
+                        synchronized (threadErrors) {
+                            threadErrors.add(e);
+                        }
+                    }
+                });
+            }
+            // Execute all threads and then proceed, or shut down on fail(s)
+            CompletableFuture.allOf(aPICallStoreThreads).join();
+            if (!threadErrors.isEmpty()) {
+                System.err.println("Error in application startup (database population stage 2): ");
+                threadErrors.forEach(e -> System.err.println(e.getMessage()));
+                System.exit(1);
             }
         }
+        // Shut down if db population fails
         catch (RuntimeException | SQLException | IOException e) {
-            System.err.println("Error in application startup (database population)" + e.getMessage());
+            System.err.println("Error in application startup (database population stage 1): "
+                    + e.getMessage());
+            System.exit(1);
         }
 
         launch();
